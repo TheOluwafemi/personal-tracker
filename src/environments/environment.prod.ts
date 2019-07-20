c71b041f85467dfbc3b2eddb606a063776e37bbc
theOluwafemi <oluwafemidamilola21@gmail.com>
2019-07-21 00:07:59 +0100

    environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCy2y9ipeblvB2M19i1COwVKuaPF7SJBIQ',
+    authDomain: 'tracker-247218.firebaseapp.com',
+    databaseURL: 'https://tracker-247218.firebaseio.com',
+    projectId: 'tracker-247218',
+    storageBucket: 'tracker-247218.appspot.com',
+    messagingSenderId: '1044015455009',
+    appId: '1:1044015455009:web:217288ced2ccde83'
+  },
 };
